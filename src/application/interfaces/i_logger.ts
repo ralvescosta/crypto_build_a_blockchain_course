fdffe6c8f77e03773f2a910c2d116b02b33ef03f
ralvescosta <rafael.rac.mg@gmail.com>
2022-01-31 06:15:13 -0300

    refactor: logger

--- src/application/interfaces/i_logger.ts
+++ src/application/interfaces/i_logger.ts
@@ -1,6 +1,8 @@
-export default interface ILogger {
+interface ILogger {
   trace: (d: any) => void
   info: (d: any) => void
   warn: (d: any) => void
   error: (d: any) => void
 }
+
+export { ILogger }

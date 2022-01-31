5ac67d86b32f71a988a63df2c1e643ddb6e5c7ec
ralvescosta <rafael.rac.mg@gmail.com>
2022-01-31 06:15:43 -0300

    refactor: http server

--- src/infra/http_server/http_server.ts
+++ src/infra/http_server/http_server.ts
@@ -2,11 +2,11 @@ import express from 'express'
 import cors from 'cors'
 import helmet from 'helmet'
 import compression from 'compression'
-import ILogger from '../../application/interfaces/i_logger'
+import { ILogger } from '../../application/interfaces/i_logger'
 
 export const server = express()
 class HttpServer {
-  constructor (private readonly _logger: ILogger) {}
+  constructor (private readonly logger: ILogger) {}
 
   public setup (): void {
    server.use(cors())
@@ -16,13 +16,15 @@ class HttpServer {
   }
 
   public registerRoute (method: string, path: string, handler: any): void {
-    this._logger.info(`[Server::Router] Method: ${method.toUpperCase()} | Path: /api/${path}`);
+    this.logger.info(`[HttpServer::Router] Method: ${method.toUpperCase()} | Path: /api${path}`);
     (server as any)[method](path, handler)
   }
 
   public run (): void {
     const port = process.env.PORT || 3333
-    server.listen(port, () => this._logger.info({ message: `server running on port ${port}` }))
+    server.listen(port, () => {
+      this.logger.info(`[HttpServer::Runner] server running on port ${port}`)
+    })
   }
 }
 

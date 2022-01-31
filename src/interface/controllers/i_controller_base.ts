c00bdbb7248ef5cb4b271537b0842b416812a68e
ralvescosta <rafael.rac.mg@gmail.com>
2022-01-31 06:15:30 -0300

    feat: controller and router interface

---
+++ src/interface/controllers/i_controller_base.ts
@@ -0,0 +1,12 @@
+import { HttpRequest, HttpResponse } from '../../infra/http_server/http'
+
+interface IControllerBase {
+  create: (httpRequest: HttpRequest) => Promise<HttpResponse>
+  findOne: (httpRequest: HttpRequest) => Promise<HttpResponse>
+  findAll: (httpRequest: HttpRequest) => Promise<HttpResponse>
+  update: (httpRequest: HttpRequest) => Promise<HttpResponse>
+  delete: (httpRequest: HttpRequest) => Promise<HttpResponse>
+  findOrCreate: (httpRequest: HttpRequest) =>Promise<HttpResponse>
+}
+
+export { IControllerBase }
---
+++ src/shared/i_roter.ts
@@ -0,0 +1,5 @@
+interface IRouter {
+  register: () => void
+}
+
+export { IRouter }

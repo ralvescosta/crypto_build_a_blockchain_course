b0c689be15f8bb07b87c7eaba90f0d0142f16940
ralvescosta <rafael.rac.mg@gmail.com>
2022-01-31 06:15:52 -0300

    refactor: ioc

--- src/ioc.ts
+++ src/ioc.ts
@@ -1,18 +1,22 @@
 import { createContainer, InjectionMode, asValue, AwilixContainer, asClass } from 'awilix'
 import pino from 'pino'
-import pinoInspector from 'pino-inspector'
+import pretty from 'pino-pretty'
 
 import { HttpResponseFactory } from './shared/http_response_factory'
 import { HttpServer } from './infra/http_server/http_server'
 
+import { Routes } from './interface/presenters/routes'
+
 export const container = createContainer({
-  injectionMode: InjectionMode.PROXY
+  injectionMode: InjectionMode.CLASSIC
 })
 export const registerInjections = (): AwilixContainer => {
   container.register({
     logger: asValue(createLoggerInstance()),
     httpServer: asClass(HttpServer).singleton(),
-    httpResponseFactory: asClass(HttpResponseFactory).singleton()
+    httpResponseFactory: asClass(HttpResponseFactory).singleton(),
+
+    Routes: asClass(Routes).singleton()
   })
 
   return container
@@ -23,12 +27,13 @@ const createLoggerInstance = (): pino.Logger => {
   const debug = process.env.DEBUG === 'true'
 
   if (debug) {
+    const stream = pretty({
+      colorize: true
+    })
     logger = pino({
       enabled: process.env.ENABLE_LOG === 'true',
-      level: process.env.LOG_LEVEL || 'warn',
-      prettyPrint: true,
-      prettifier: pinoInspector
-    })
+      level: process.env.LOG_LEVEL || 'warn'
+    }, stream)
   } else {
     logger = pino({
       enabled: process.env.ENABLE_LOG === 'true',

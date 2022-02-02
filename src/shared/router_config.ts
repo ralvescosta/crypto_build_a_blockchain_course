286292b884bc60b3f48848e1781bb6419d108bcb
ralvescosta <rafael.rac.mg@gmail.com>
2022-02-02 06:55:46 -0300

    refactor: change app folders

--- src/shared/router_config.ts
+++
@@ -1,9 +0,0 @@
-const routerConfig = (cradle: any): void => {
-  Object.keys(cradle).forEach((key: string) => {
-    if (key.includes('Routes')) {
-      cradle[key].register()
-    }
-  })
-}
-
-export { routerConfig }

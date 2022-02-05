3bcca6bc0cb52fa2ecf4afb947c96c52f65a2bc3
ralvescosta <rafael.rac.mg@gmail.com>
2022-02-05 10:04:21 -0300

    refactor: p2p server

--- src/infra/websocket/p2p_server.ts
+++ src/infra/websocket/p2p_server.ts
@@ -1,6 +1,7 @@
 import WsClient, { Server, WebSocket } from 'ws'
 import { ILogger } from '../../application/interfaces/i_logger'
-import { IBlockchainRepository } from '../../application/interfaces/i_blockchain_repository'
+import { ISyncBlockchainUseCase } from '../../domain/usecases/i_sync_blockchain_usecase'
+import { IGetBlocksUseCase } from '../../domain/usecases/i_get_blocks_usecase'
 
 class P2PServer {
   private readonly PEARS_ADDRESS: string[]
@@ -8,7 +9,8 @@ class P2PServer {
   private readonly _connectedNodes: WebSocket[] = []
   constructor (
     private readonly logger: ILogger,
-    private readonly blockchainRepository: IBlockchainRepository
+    private readonly syncBlockchainUseCase: ISyncBlockchainUseCase,
+    private readonly getBlocksUseCase: IGetBlocksUseCase
   ) {
     this.PEARS_ADDRESS = process.env.WS_PEERS_ADDRESS ? process.env.WS_PEERS_ADDRESS.split(',') : []
     this.WS_PORT = Number(process.env.WS_PORT || 5001)
@@ -31,9 +33,34 @@ class P2PServer {
     })
   }
 
+  public syncChains (): void {
+    if (!this.PEARS_ADDRESS.length) {
+      this.logger.info('[P2PServer::coonToNetwork] - Dont have node address to connect')
+      return
+    }
+
+    this._connectedNodes.forEach(connection => this._sendChain(connection))
+  }
+
   private _connectSocket (connection: WebSocket): void {
     this._connectedNodes.push(connection)
     this.logger.info('[SP2PServer::_connectSocket] - socket connected')
+
+    this._handleMessage(connection)
+    this._sendChain(connection)
+  }
+
+  private _handleMessage (connection: WebSocket): void {
+    connection.on('message', message => {
+      const chain = JSON.parse(message.toString())
+
+      this.syncBlockchainUseCase.sync(chain)
+    })
+  }
+
+  private async _sendChain (connection: WebSocket): Promise<void> {
+    const chain = await this.getBlocksUseCase.perform()
+    connection.send(JSON.stringify(chain.value))
   }
 }
 

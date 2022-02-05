d7b516e33d62a461b3c5a996efd4f768616a7653
ralvescosta <rafael.rac.mg@gmail.com>
2022-02-05 10:05:29 -0300

    refactor: controller

--- src/interface/http/controllers/blockchain_controller.ts
+++ src/interface/http/controllers/blockchain_controller.ts
@@ -1,17 +1,29 @@
 import { HttpRequest, HttpResponse } from '../../../infra/http_server/http'
-import { IGetBlocksUseCase } from '../../../domain/usecases/i_get_blocks_usecases'
+import { IGetBlocksUseCase } from '../../../domain/usecases/i_get_blocks_usecase'
+import { IMineUseCase } from '../../../domain/usecases/i_mine_usecase'
 import { IHttpResponseFactory } from '../factories/i_http_response_factory'
 import { IHttpErrorHandlerFactory } from '../factories/i_http_error_handler_factory'
 
 class BlockchainController {
   constructor (
     private readonly getBlocksUseCase: IGetBlocksUseCase,
+    private readonly mineUseCase: IMineUseCase,
     private readonly httpResponseFactory: IHttpResponseFactory,
     private readonly httpErrorHandlerFactory: IHttpErrorHandlerFactory) {}
 
     public async getBlocks (_: HttpRequest): Promise<HttpResponse> {
       const result = await this.getBlocksUseCase.perform()
       if (result.isLeft()) {
+        return this.httpErrorHandlerFactory.handler(result)
+      }
+
+      return this.httpResponseFactory.ok({ body: result.value })
+    }
+
+    public async mine (request: HttpRequest): Promise<HttpResponse> {
+      const { data: dataToMine } = request.body
+      const result = await this.mineUseCase.perform(dataToMine)
+       if (result.isLeft()) {
         return this.httpErrorHandlerFactory.handler(result)
       }
 

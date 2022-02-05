622e2f7eef0225e2cbb343591f08958828e5aa0d
ralvescosta <rafael.rac.mg@gmail.com>
2022-02-05 10:05:08 -0300

    refactor: get blocks

--- src/domain/usecases/i_get_blocks_usecase.ts
+++ src/domain/usecases/i_get_blocks_usecase.ts
@@ -1,6 +1,6 @@
 import { BaseError } from '../../shared/base_error'
 import { Either } from '../../shared/either'
-import { Block } from '../../domain/entities/block'
+import { Block } from '../entities/block'
 
 interface IGetBlocksUseCase {
    perform (): Promise<Either<BaseError, [] | Block[]>>

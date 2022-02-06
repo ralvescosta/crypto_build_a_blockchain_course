ad4af9ce139d25c12c5701509ff44cfc40ac15fc
ralvescosta <rafael.rac.mg@gmail.com>
2022-02-06 17:21:42 -0300

    refactor: mine usecase

--- src/application/usecases/mine_usecase.ts
+++ src/application/usecases/mine_usecase.ts
@@ -1,22 +1,25 @@
 import { Block } from '../../domain/entities/block'
 import { BaseError } from '../../shared/base_error'
-import { Either, left, right } from '../../shared/either'
+import { Either, left } from '../../shared/either'
 import { IBlockchainRepository } from '../interfaces/i_blockchain_repository'
 import { IMineUseCase } from '../../domain/usecases/i_mine_usecase'
+import { IGetBlocksUseCase } from '../../domain/usecases/i_get_blocks_usecase'
 
 class MineUseCase implements IMineUseCase {
-  constructor (private readonly blockchainRepository: IBlockchainRepository) {}
+  constructor (
+    private readonly blockchainRepository: IBlockchainRepository,
+    private readonly getBlocksUseCase: IGetBlocksUseCase
+  ) {}
 
-  public async perform (data: string[]): Promise<Either<BaseError, Block>> {
-    const bc = await this.blockchainRepository.getBlockchain()
-    if (bc.isLeft()) {
-      return left(bc.value)
+  public async perform (data: string[]): Promise<Either<BaseError, Block[]>> {
+    const blockchain = await this.blockchainRepository.getBlockchain()
+    if (blockchain.isLeft()) {
+      return left(blockchain.value)
     }
 
-    const blockchain = bc.value
-    const createdBlock = blockchain.addBlock(data) as Block
+    blockchain.value.addBlock(data) as Block
 
-    return right(createdBlock)
+    return this.getBlocksUseCase.perform()
   }
 }
 
--- src/domain/usecases/i_mine_usecase.ts
+++ src/domain/usecases/i_mine_usecase.ts
@@ -3,7 +3,7 @@ import { BaseError } from '../../shared/base_error'
 import { Either } from '../../shared/either'
 
 interface IMineUseCase {
-  perform: (data: string[]) => Promise<Either<BaseError, Block>>
+  perform: (data: string[]) => Promise<Either<BaseError, Block[]>>
 }
 
 export { IMineUseCase }

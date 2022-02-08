20522db796cdcedee08edea0be44a9fe4817ae3d
ralvescosta <rafael.rac.mg@gmail.com>
2022-02-08 04:44:13 -0300

    refactor: mining

--- src/domain/entities/block.ts
+++ src/domain/entities/block.ts
@@ -1,44 +1,53 @@
 import SHA256 from 'crypto-js/sha256'
 class Block {
+  static DIFFICULTY: number = 4
+
   constructor (
     public timestamp: number,
     public lastHash: string,
     public hash: string,
-    public data: string[]
+    public data: string[],
+    public nonce: number
   ) {}
 
   public toString () {
     return `[Block]
       Timestamp: ${this.timestamp}
       Last Hash: ${this.lastHash}
-      Hash: ${this.hash}
-      Data: ${this.data}
+      Hash     : ${this.hash}
+      Nonce    : ${this.nonce}
+      Data     : ${this.data}
   `
   }
 
   static genesis (): Block {
-    const timestamp = Date.now()
     const data = ['Genesis']
-
-    return new this(Date.now(), '', this.hash(timestamp, '', data), data)
+    return this.mineBlock({ hash: '' } as any, data)
   }
 
   static mineBlock (lastBlock: Block, data: string[]): Block {
-    const timestamp = Date.now()
     const lastHash = lastBlock.hash
-    const hash = this.hash(timestamp, lastHash, data)
+    let nonce = 0
+    let timestamp = Date.now()
+    let hash = this.hash(timestamp, lastHash, data, nonce)
 
-    return new this(timestamp, lastHash, hash, data)
+    while (hash.slice(0, this.DIFFICULTY) !== '0'.repeat(this.DIFFICULTY)) {
+      nonce++
+      timestamp = Date.now()
+      hash = this.hash(timestamp, lastHash, data, nonce)
+    }
+
+    return new this(timestamp, lastHash, hash, data, nonce)
   }
 
-  static hash (timestamp: number, lastHash: string, data: string[]): string {
-    return SHA256(`${timestamp}${lastHash}${data.join()}`).toString()
+  static hash (timestamp: number, lastHash: string, data: string[], nonce: number): string {
+    return SHA256(`${timestamp}${lastHash}${data.join()}${nonce}`).toString()
   }
 
   static blockHash (block: Block): string {
-    const { timestamp, lastHash, data } = block
+    const { timestamp, lastHash, data, nonce } = block
 
-    return this.hash(timestamp, lastHash, data)
+    return this.hash(timestamp, lastHash, data, nonce)
   }
 }
 

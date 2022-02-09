d7e924b5bcf9d6d5542e802319384be07c0a1f0a
ralvescosta <rafael.rac.mg@gmail.com>
2022-02-09 05:26:11 -0300

    feat: pub and private keys

---
+++ src/shared/keys.ts
@@ -0,0 +1,16 @@
+import { ec as EC } from 'elliptic'
+
+class Keys {
+  private static elliptic: EC = new EC('secp257k1')
+
+  static genKeyPair () {
+    const keyPair = this.elliptic.genKeyPair()
+
+    return {
+      publicKey: keyPair.getPublic().encode('hex', false),
+      privateKey: keyPair.getPrivate().toString()
+    }
+  }
+}
+
+export { Keys }
